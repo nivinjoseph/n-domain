c02cd68731164a9dc87430379866eb08b8c6db92
nivinjoseph <nivinjoseph@outlook.com>
2020-05-08 13:30:19 -0400

    refactorings

--- src/domain-helper.ts
+++ src/domain-helper.ts
@@ -1,5 +1,5 @@
 import "@nivinjoseph/n-ext";
-import { Uuid, Make } from "@nivinjoseph/n-util";
+import { Uuid } from "@nivinjoseph/n-util";
 import { given } from "@nivinjoseph/n-defensive";
 
 // public
@@ -11,14 +11,10 @@ export class DomainHelper
     public static generateId(prefix: string): string
     {
         given(prefix, "prefix").ensureHasValue().ensureIsString()
-            .ensure(t => t.trim().length >= 3 && t.trim().length <= 7, "should be between 3 and 7 chars long")
-            .ensure(t =>
-            {
-                const format: string[] = [];
-                Make.loop(() => format.push("@"), t.trim().length);
-                return t.trim().matchesFormat(format.join(""));
-            }, "should contain only alphabet chars");
+            .ensure(t => t.trim().length === 3, "should be 3 chars long")
+            .ensure((t) => t.matchesFormat("@@@"), "should contain only alphabet chars");
         
+        // 4 + 32 = 36
         return `${prefix.trim().toLowerCase()}_${Uuid.create().replaceAll("-", "")}`;
     }
 }
