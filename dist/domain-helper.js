10ee83ea4bf67fd0c0bbd5a11d289036d03664d1
nivinjoseph <nivinjoseph@outlook.com>
2020-05-08 13:30:37 -0400

    preparing to publish new version

--- dist/domain-helper.js
+++ dist/domain-helper.js
@@ -8,12 +8,9 @@ class DomainHelper {
     static get now() { return Date.now(); }
     static generateId(prefix) {
         n_defensive_1.given(prefix, "prefix").ensureHasValue().ensureIsString()
-            .ensure(t => t.trim().length >= 3 && t.trim().length <= 7, "should be between 3 and 7 chars long")
-            .ensure(t => {
-            const format = [];
-            n_util_1.Make.loop(() => format.push("@"), t.trim().length);
-            return t.trim().matchesFormat(format.join(""));
-        }, "should contain only alphabet chars");
+            .ensure(t => t.trim().length === 3, "should be 3 chars long")
+            .ensure((t) => t.matchesFormat("@@@"), "should contain only alphabet chars");
+        // 4 + 32 = 36
         return `${prefix.trim().toLowerCase()}_${n_util_1.Uuid.create().replaceAll("-", "")}`;
     }
 }
