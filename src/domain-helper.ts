45d3a8a5c9da3f29a52224c200742a0814860a5d
nivinjoseph <nivinjoseph@outlook.com>
2020-05-08 12:39:55 -0400

    tightened rules around id

--- src/domain-helper.ts
+++ src/domain-helper.ts
@@ -1,5 +1,5 @@
 import "@nivinjoseph/n-ext";
-import { Uuid } from "@nivinjoseph/n-util";
+import { Uuid, Make } from "@nivinjoseph/n-util";
 import { given } from "@nivinjoseph/n-defensive";
 
 // public
@@ -11,7 +11,13 @@ export class DomainHelper
     public static generateId(prefix: string): string
     {
         given(prefix, "prefix").ensureHasValue().ensureIsString()
-            .ensure(t => t.trim().length >= 3 && t.trim().length <= 7, "should be between 3 and 7 chars long");
+            .ensure(t => t.trim().length >= 3 && t.trim().length <= 7, "should be between 3 and 7 chars long")
+            .ensure(t =>
+            {
+                const format: string[] = [];
+                Make.loop(() => format.push("@"), t.trim().length);
+                return t.trim().matchesFormat(format.join(""));
+            }, "should contain only alphabet chars");
         
         return `${prefix.trim().toLowerCase()}_${Uuid.create().replaceAll("-", "")}`;
     }
