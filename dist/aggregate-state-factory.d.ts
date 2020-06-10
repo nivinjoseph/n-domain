cdafd1509bdb4b0d78f956a67322a03f63e21cb0
nivinjoseph <nivinjoseph@outlook.com>
2020-06-10 14:53:26 -0400

    preparing to publish new version

--- dist/aggregate-state-factory.d.ts
+++ dist/aggregate-state-factory.d.ts
@@ -1,7 +1,7 @@
 import { AggregateState } from "./aggregate-state";
 export declare abstract class AggregateStateFactory<T extends AggregateState> {
     abstract create(): T;
-    abstract update(state: T): T;
-    abstract deserializeSnapshot(snapshot: T): T;
+    update(state: T): T;
+    deserializeSnapshot(snapshot: T): T;
     protected createDefaultAggregateState(): AggregateState;
 }
