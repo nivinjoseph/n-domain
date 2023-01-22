3a4e70d8853bfd916fd65c2c42890c83598fcc7b
Nivin Joseph <nivinjoseph@outlook.com>
2023-01-22 11:04:04 -0500

    ensured ulid is monotonic

--- src/domain-helper.ts
+++ src/domain-helper.ts
@@ -1,7 +1,8 @@
 // import { Uuid } from "@nivinjoseph/n-util";
 import { given } from "@nivinjoseph/n-defensive";
-import { ulid } from "ulid";
-
+// import { ulid } from "ulid";
+import { monotonicFactory } from "ulid";
+const ulid = monotonicFactory();
 
 // public
 export class DomainHelper
@@ -24,7 +25,7 @@ export class DomainHelper
         given(prefix, "prefix").ensureHasValue().ensureIsString()
             .ensure(t => t.trim().length === 3, "should be 3 chars long")
             .ensure((t) => t.matchesFormat("@@@"), "should contain only alphabet chars");
-
+        
         const date = new Date();
         const year = date.getUTCFullYear().toString().substring(2);
         const month = ((date.getUTCMonth() + 1) / 100).toFixed(2).split(".").takeLast();
@@ -32,9 +33,7 @@ export class DomainHelper
         
         const dateValue = `${year}${month}${day}`;
         
-        console.log(dateValue);
-        
         // 4 + 32 = 36
-        return `${prefix.trim().toLowerCase()}_${dateValue}${ulid()}`.toLowerCase();
+        return `${prefix.trim().toLowerCase()}_${dateValue}${ulid(date.valueOf())}`.toLowerCase();
     }
 }
--- test/domain-helper.test.ts
+++ test/domain-helper.test.ts
@@ -1,6 +1,9 @@
 import * as Assert from "assert";
 import { DomainHelper } from "../src";
 import "@nivinjoseph/n-ext";
+import { Delay } from "@nivinjoseph/n-util";
+import * as Fs from "fs";
+import * as Path from "path";
 
 
 suite("DomainHelper tests", () =>
@@ -25,5 +28,38 @@ suite("DomainHelper tests", () =>
         Assert.strictEqual(id.length, 36);
         console.log("id", id);
         console.log("max number length", Number.MAX_SAFE_INTEGER.toString().length);
+    });
+    
+    test("monotonic id", async () =>
+    {
+        const pastIdsFilePath = Path.join(__dirname, "past-ids.json");
+        let pastIds = new Array<string>();
+        if (Fs.existsSync(pastIdsFilePath))
+            pastIds = JSON.parse(Fs.readFileSync(pastIdsFilePath, "utf-8"));
+        
+        const ids = new Array<string>();
+        
+        for (let i = 0; i < 100; i++)
+        {
+            ids.push(DomainHelper.generateId("tst"));
+            
+            if (i % 10 === 0)
+                await Delay.seconds(1);        
+        }
+        
+        const combinedIds = [...pastIds, ...ids];
+        
+        const ordered = combinedIds.orderBy();
+        let isOrdered = true;
+        for (let i = 0; i < combinedIds.length; i++)
+        {
+            if (combinedIds[i] !== ordered[i])
+                isOrdered = false;
+        }
+        
+        Fs.writeFileSync(pastIdsFilePath, JSON.stringify(ids), "utf-8");
+        
+        Assert.ok(combinedIds.equals(combinedIds.orderBy()));
+        Assert.ok(isOrdered);
     });
 });
