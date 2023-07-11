721f5711bcc5e82391e3ea736f335371269e505f
Nivin Joseph <nivinjoseph@outlook.com>
2023-07-11 06:08:21 -0400

    preparing to publish new version

--- dist/domain-helper.d.ts
+++ dist/domain-helper.d.ts
@@ -1,5 +1,6 @@
 export declare class DomainHelper {
     static get now(): number;
     static generateId(prefix: string): string;
+    static aggregateTypeToSnakeCase(aggregateType: Function): string;
 }
 //# sourceMappingURL=domain-helper.d.ts.map
