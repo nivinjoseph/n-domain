bfca57c9ca69425aef77ac1fe51eff67742c677c
Nivin Joseph <nivinjoseph@outlook.com>
2023-07-13 03:53:47 -0400

    Added ConfigurableDomainContext

---
+++ src/configurable-domain-context.ts
@@ -0,0 +1,19 @@
+import { given } from "@nivinjoseph/n-defensive";
+import { DomainContext } from "./domain-context";
+
+
+export class ConfigurableDomainContext implements DomainContext
+{
+    private _userId: string;
+
+    public get userId(): string { return this._userId; }
+    public set userId(value: string) { this._userId = value; }
+    
+    
+    public constructor(userId: string)
+    {
+        given(userId, "userId").ensureHasValue().ensureIsString();
+        this._userId = userId;
+    }
+}
+
